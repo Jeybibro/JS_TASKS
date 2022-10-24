b1d6a902c90b309aaac62226a3679ba61e4b72bb
JeyBi <jeybibro@gmail.com>
2022-10-24 08:54:44 +0600

    init: start variables

---
+++ Variables/script.js
@@ -0,0 +1,13 @@
+let nameNumber = '1',
+    NameNumber = '2',
+    name_number = '3';
+alert(nameNumber);
+alert(NameNumber);
+alert(name_number);
+
+const objectA = {
+    a = 20,
+    b = true
+}
+
+const copyObjA = objectA;

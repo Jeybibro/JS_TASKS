a8da78e04111e05b24b7a4459b3a20b1ec4aabde
JeyBi <jeybibro@gmail.com>
2022-10-24 09:19:07 +0600

    feat:

--- Variables/script.js
+++ Variables/script.js
@@ -1,6 +1,6 @@
-let nameNumber = '1',
-    NameNumber = '2',
-    name_number = '3';
-alert(nameNumber);
+let nameNumber = 1,
+    NameNumber = 2,
+    name_number = 3;
+alert(nameNumber,);
 alert(NameNumber);
 alert(name_number);

db66e5a27a1594f68ef07dc9cc4d532e2070236f
JeyBi <jeybibro@gmail.com>
2022-10-26 08:37:12 +0600

    added option2

--- 24-10/script.js
+++ 24-10/script.js
@@ -1,7 +1,21 @@
-let name = 'Бахтияр',
-    age = 27,
-    str1 = 'САЛАМ ',
-    str2='БИШКЕК!';
-alert('Привет '+name);
+//Первый вариант просто додумал на скорую руку
+let name = 'Бахтияр';
+let    age = 27;
+let    str1 = 'САЛАМ, ';
+let    str2='БИШКЕК!';
+alert('Привет, '+name);
 alert('Мне '+age+' лет');
 alert(str1+str2);
+
+//второй вариант по материалу к чтению
+let nameJey = 'JeyBi';
+let hay = `Hello, ${nameJey}`;
+alert(hay);
+
+let ageJey = 27;
+let age_jey = `Мне ${age} лет!`;
+alert(age_jey);
+
+let str_1 = 'БИШКЕК!';
+let str_2 = `САЛАМ, ${str_1}`;
+alert(str_2);

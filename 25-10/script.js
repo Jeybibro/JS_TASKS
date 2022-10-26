11be746ea3817bc1a357b4abf91af7b1e84c09c4
JeyBi <jeybibro@gmail.com>
2022-10-26 08:47:46 +0600

    init:25-10

---
+++ 25-10/script.js
@@ -0,0 +1,21 @@
+//Первое задание
+let a = 17;
+let b = 10;
+let c = a - b;
+let d = 7;
+let result = c + d;
+console.log(result);
+
+//Второе задание
+let hour, minute, second;
+hour = 8;
+minute = 44;
+second = 09;
+let time = `${hour}:${minute}:${second}`;
+console.log(time);
+
+//Третее задание
+let num = 10;
+num = ++num;
+num = ++num;
+alert(num);

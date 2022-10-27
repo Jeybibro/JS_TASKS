483767d5e4c53200b65e80a4ada21d827d49ceea
JeyBi <jeybibro@gmail.com>
2022-10-27 20:30:44 +0600

    feat:added 27-10

---
+++ 27-10/script.js
@@ -0,0 +1,23 @@
+//№1
+let city = prompt('„Столица Кыргызстана?“', '');
+let new_city = city === 'Бишкек' ? 'Верно!' : 'Не знаете?';
+alert(new_city);
+
+
+
+//№2
+let user_pass = prompt('Логин','');
+if (user_pass == null){
+    alert('Вход отменен');
+}else if(user_pass === 'Админ'){
+    user_pass = prompt('Пароль','');
+    if (user_pass == null){
+        alert('Вход отменен');
+    }else if(user_pass === 'Черный Властелин'){
+        alert('Добро пожаловать');
+    }else{
+        alert('Пароль неверен!');
+    }
+}else {
+    alert('Я вас не знаю!');
+}
